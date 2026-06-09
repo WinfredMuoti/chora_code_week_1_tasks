5de1685b7995621aada69e7be0f19cf514341b1f
Joseph Kyalo <kyalo@cloudkite.io>
2026-06-09 15:30:10 +0300

    chore: refactor Task persistence and loading to accomodate Datetime objects

--- storage.py
+++ storage.py
@@ -1,16 +1,15 @@
 import json
 from pathlib import Path
 from dataclasses import asdict
+from datetime import datetime
 
 from tasks import Task, TaskList
 
 
 def save_tasks(task_list: TaskList, path: Path) -> None:
     data = [asdict(task) for task in task_list._tasks]
-
     with path.open("w") as file:
-        json.dump(data, file, indent=2)
-
+        json.dump(data, file, indent=2, default=str)
 
 def load_tasks(path: Path) -> TaskList:
     with path.open("r") as file:
@@ -22,7 +21,8 @@ def load_tasks(path: Path) -> TaskList:
         Task(
             title=item["title"],
             done=item["done"],
-            id=item["id"]
+            id=item["id"],
+            created_at=datetime.fromisoformat(item["created_at"]),
         )
         for item in data
     ]
